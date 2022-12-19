e6af3f68fea8bab5a932153a7f6a372d4c242b88
Mikael Johansson <mik.json@gmail.com>
2022-12-19 13:36:03 +0100

    Use imported API types instead

--- source/f5_virtualserver.go
+++ source/f5_virtualserver.go
@@ -35,6 +35,7 @@ import (
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/tools/cache"
 
+	f5 "github.com/F5Networks/k8s-bigip-ctlr/v2/config/apis/cis/v1"
 	"sigs.k8s.io/external-dns/endpoint"
 )
 
@@ -101,14 +102,14 @@ func (vs *f5VirtualServerSource) Endpoints(ctx context.Context) ([]*endpoint.End
 		return nil, err
 	}
 
-	var virtualServers []*VirtualServer
+	var virtualServers []*f5.VirtualServer
 	for _, vsObj := range virtualServerObjects {
 		unstructuredHost, ok := vsObj.(*unstructured.Unstructured)
 		if !ok {
 			return nil, errors.New("could not convert")
 		}
 
-		virtualServer := &VirtualServer{}
+		virtualServer := &f5.VirtualServer{}
 		err := vs.unstructuredConverter.scheme.Convert(unstructuredHost, virtualServer, nil)
 		if err != nil {
 			return nil, err
@@ -141,7 +142,7 @@ func (vs *f5VirtualServerSource) AddEventHandler(ctx context.Context, handler fu
 }
 
 // endpointsFromVirtualServers extracts the endpoints from a slice of VirtualServers
-func (vs *f5VirtualServerSource) endpointsFromVirtualServers(virtualServers []*VirtualServer) ([]*endpoint.Endpoint, error) {
+func (vs *f5VirtualServerSource) endpointsFromVirtualServers(virtualServers []*f5.VirtualServer) ([]*endpoint.Endpoint, error) {
 	var endpoints []*endpoint.Endpoint
 
 	for _, virtualServer := range virtualServers {
@@ -193,7 +194,7 @@ func newVSUnstructuredConverter() (*unstructuredConverter, error) {
 	}
 
 	// Add the core types we need
-	uc.scheme.AddKnownTypes(f5VirtualServerGVR.GroupVersion(), &VirtualServer{}, &VirtualServerList{})
+	uc.scheme.AddKnownTypes(f5VirtualServerGVR.GroupVersion(), &f5.VirtualServer{}, &f5.VirtualServerList{})
 	if err := scheme.AddToScheme(uc.scheme); err != nil {
 		return nil, err
 	}
@@ -202,7 +203,7 @@ func newVSUnstructuredConverter() (*unstructuredConverter, error) {
 }
 
 // filterByAnnotations filters a list of VirtualServers by a given annotation selector.
-func (vs *f5VirtualServerSource) filterByAnnotations(virtualServers []*VirtualServer) ([]*VirtualServer, error) {
+func (vs *f5VirtualServerSource) filterByAnnotations(virtualServers []*f5.VirtualServer) ([]*f5.VirtualServer, error) {
 	labelSelector, err := metav1.ParseToLabelSelector(vs.annotationFilter)
 	if err != nil {
 		return nil, err
@@ -218,7 +219,7 @@ func (vs *f5VirtualServerSource) filterByAnnotations(virtualServers []*VirtualSe
 		return virtualServers, nil
 	}
 
-	filteredList := []*VirtualServer{}
+	filteredList := []*f5.VirtualServer{}
 
 	for _, vs := range virtualServers {
 		// convert the VirtualServer's annotations to an equivalent label selector
@@ -233,192 +234,6 @@ func (vs *f5VirtualServerSource) filterByAnnotations(virtualServers []*VirtualSe
 	return filteredList, nil
 }
 
-func (vs *f5VirtualServerSource) setResourceLabel(virtualServer *VirtualServer, ep *endpoint.Endpoint) {
+func (vs *f5VirtualServerSource) setResourceLabel(virtualServer *f5.VirtualServer, ep *endpoint.Endpoint) {
 	ep.Labels[endpoint.ResourceLabelKey] = fmt.Sprintf("f5-virtualserver/%s/%s", virtualServer.Namespace, virtualServer.Name)
-}
-
-//
-// The below structs and methods have been copy-pasted from
-// https://github.com/F5Networks/k8s-bigip-ctlr/blob/master/config/apis/cis/v1/types.go
-// since there's no way of importing these at the moment,
-// please see this issue for more info: https://github.com/F5Networks/k8s-bigip-ctlr/issues/2153.
-// When and if this issue can be fixed we can instead import these types.
-//
-
-// VirtualServer defines the VirtualServer resource.
-type VirtualServer struct {
-	metav1.TypeMeta   `json:",inline"`
-	metav1.ObjectMeta `json:"metadata,omitempty"`
-
-	Spec   VirtualServerSpec   `json:"spec"`
-	Status VirtualServerStatus `json:"status,omitempty"`
-}
-
-// VirtualServerStatus is the status of the VirtualServer resource.
-type VirtualServerStatus struct {
-	VSAddress string `json:"vsAddress,omitempty"`
-}
-
-// VirtualServerSpec is the spec of the VirtualServer resource.
-type VirtualServerSpec struct {
-	Host                   string           `json:"host,omitempty"`
-	VirtualServerAddress   string           `json:"virtualServerAddress,omitempty"`
-	IPAMLabel              string           `json:"ipamLabel,omitempty"`
-	VirtualServerName      string           `json:"virtualServerName,omitempty"`
-	VirtualServerHTTPPort  int32            `json:"virtualServerHTTPPort,omitempty"`
-	VirtualServerHTTPSPort int32            `json:"virtualServerHTTPSPort,omitempty"`
-	Pools                  []Pool           `json:"pools,omitempty"`
-	TLSProfileName         string           `json:"tlsProfileName,omitempty"`
-	HTTPTraffic            string           `json:"httpTraffic,omitempty"`
-	SNAT                   string           `json:"snat,omitempty"`
-	WAF                    string           `json:"waf,omitempty"`
-	RewriteAppRoot         string           `json:"rewriteAppRoot,omitempty"`
-	AllowVLANs             []string         `json:"allowVlans,omitempty"`
-	IRules                 []string         `json:"iRules,omitempty"`
-	ServiceIPAddress       []ServiceAddress `json:"serviceAddress,omitempty"`
-}
-
-// ServiceAddress Service IP address definition (BIG-IP virtual-address).
-type ServiceAddress struct {
-	ArpEnabled         bool   `json:"arpEnabled,omitempty"`
-	ICMPEcho           string `json:"icmpEcho,omitempty"`
-	RouteAdvertisement string `json:"routeAdvertisement,omitempty"`
-	TrafficGroup       string `json:"trafficGroup,omitempty"`
-	SpanningEnabled    bool   `json:"spanningEnabled,omitempty"`
-}
-
-// Pool defines a pool object in BIG-IP.
-type Pool struct {
-	Path            string  `json:"path,omitempty"`
-	Service         string  `json:"service"`
-	ServicePort     int32   `json:"servicePort"`
-	NodeMemberLabel string  `json:"nodeMemberLabel,omitempty"`
-	Monitor         Monitor `json:"monitor"`
-	Rewrite         string  `json:"rewrite,omitempty"`
-}
-
-// Monitor defines a monitor object in BIG-IP.
-type Monitor struct {
-	Type     string `json:"type"`
-	Send     string `json:"send"`
-	Recv     string `json:"recv"`
-	Interval int    `json:"interval"`
-	Timeout  int    `json:"timeout"`
-}
-
-// VirtualServerList is a list of the VirtualServer resources.
-type VirtualServerList struct {
-	metav1.TypeMeta `json:",inline"`
-	metav1.ListMeta `json:"metadata"`
-
-	Items []VirtualServer `json:"items"`
-}
-
-// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
-func (in *VirtualServer) DeepCopyInto(out *VirtualServer) {
-	*out = *in
-	out.TypeMeta = in.TypeMeta
-	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
-	in.Spec.DeepCopyInto(&out.Spec)
-	out.Status = in.Status
-}
-
-// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new VirtualServer.
-func (in *VirtualServer) DeepCopy() *VirtualServer {
-	if in == nil {
-		return nil
-	}
-	out := new(VirtualServer)
-	in.DeepCopyInto(out)
-	return out
-}
-
-// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
-func (in *VirtualServer) DeepCopyObject() runtime.Object {
-	if c := in.DeepCopy(); c != nil {
-		return c
-	}
-	return nil
-}
-
-// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
-func (in *VirtualServerList) DeepCopyInto(out *VirtualServerList) {
-	*out = *in
-	out.TypeMeta = in.TypeMeta
-	in.ListMeta.DeepCopyInto(&out.ListMeta)
-	if in.Items != nil {
-		in, out := &in.Items, &out.Items
-		*out = make([]VirtualServer, len(*in))
-		for i := range *in {
-			(*in)[i].DeepCopyInto(&(*out)[i])
-		}
-	}
-}
-
-// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new VirtualServerList.
-func (in *VirtualServerList) DeepCopy() *VirtualServerList {
-	if in == nil {
-		return nil
-	}
-	out := new(VirtualServerList)
-	in.DeepCopyInto(out)
-	return out
-}
-
-// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
-func (in *VirtualServerList) DeepCopyObject() runtime.Object {
-	if c := in.DeepCopy(); c != nil {
-		return c
-	}
-	return nil
-}
-
-// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
-func (in *VirtualServerSpec) DeepCopyInto(out *VirtualServerSpec) {
-	*out = *in
-	if in.Pools != nil {
-		in, out := &in.Pools, &out.Pools
-		*out = make([]Pool, len(*in))
-		copy(*out, *in)
-	}
-	if in.AllowVLANs != nil {
-		in, out := &in.AllowVLANs, &out.AllowVLANs
-		*out = make([]string, len(*in))
-		copy(*out, *in)
-	}
-	if in.IRules != nil {
-		in, out := &in.IRules, &out.IRules
-		*out = make([]string, len(*in))
-		copy(*out, *in)
-	}
-	if in.ServiceIPAddress != nil {
-		in, out := &in.ServiceIPAddress, &out.ServiceIPAddress
-		*out = make([]ServiceAddress, len(*in))
-		copy(*out, *in)
-	}
-}
-
-// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new VirtualServerSpec.
-func (in *VirtualServerSpec) DeepCopy() *VirtualServerSpec {
-	if in == nil {
-		return nil
-	}
-	out := new(VirtualServerSpec)
-	in.DeepCopyInto(out)
-	return out
-}
-
-// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
-func (in *VirtualServerStatus) DeepCopyInto(out *VirtualServerStatus) {
-	*out = *in
-}
-
-// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new VirtualServerStatus.
-func (in *VirtualServerStatus) DeepCopy() *VirtualServerStatus {
-	if in == nil {
-		return nil
-	}
-	out := new(VirtualServerStatus)
-	in.DeepCopyInto(out)
-	return out
 }
